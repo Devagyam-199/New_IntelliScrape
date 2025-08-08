a0f36018c9ff69c81666bafa542cee672be587d5
Devagya Mistry <mrx28sep2002@gmail.com>
2025-08-08 13:36:33 +0530

    title

--- Intelliscrape_Frontend/src/App.jsx
+++ Intelliscrape_Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <div>Welcome to IntelliScrape</div>
+      <div className="h-24 flex justify-center items-center font-bold text-4xl font-sans border-b-2">Welcome to IntelliScrape</div>
     </>
   );
 };
