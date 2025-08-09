ed6956c3275479811773b703904dfc7ee774da55
Devagya Mistry <mrx28sep2002@gmail.com>
2025-08-09 17:15:37 +0530

    start of frontend

--- Intelliscrape_Frontend/src/App.jsx
+++ Intelliscrape_Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const App = () => {
   return (
+    
     <>
       <div className="h-24 flex justify-center items-center font-bold text-4xl font-sans border-b-2">Welcome to IntelliScrape</div>
     </>
