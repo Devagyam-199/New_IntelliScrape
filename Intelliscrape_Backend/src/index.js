3607b0525759b841232378798dcdfff1c9c7ffef
Devagya Mistry <mrx28sep2002@gmail.com>
2025-08-11 00:52:46 +0530

    express server created

--- Intelliscrape_Backend/src/index.js
+++ Intelliscrape_Backend/src/index.js
@@ -1,6 +1,15 @@
 import dotenv from 'dotenv';
 import dbConn from './DB/dbconn.js';
+import { app } from './app.js';
 
 dotenv.config();
 
-dbConn();
+dbConn()
+.then(()=>{
+    app.listen(process.env.PORT || 8080,(req,res)=>{
+        console.log(`Server is running on port ${process.env.PORT || 8080} `);
+    })
+})
+.catch((err)=>{
+    console.error("database connection failed : ",err);
+})
